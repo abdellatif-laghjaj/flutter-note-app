421e33cf3e52c2b24b3507171b2231e70bd0db24
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2023-02-01 20:31:40 +0100

    add note to firebase

---
+++ lib/models/Note.dart
@@ -0,0 +1,15 @@
+class Note {
+  String id;
+  String title;
+  String content;
+  int color_id;
+  String date;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.color_id,
+    required this.date,
+  });
+}
--- lib/screens/add_note.dart
+++ lib/screens/add_note.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/style/app_style.dart';
 
@@ -12,8 +13,9 @@ class AddNote extends StatefulWidget {
 
 class _AddNoteState extends State<AddNote> {
   int color_id = Random().nextInt(AppStyle.cardsColor.length);
-  TextEditingController _noteTitleController = TextEditingController();
-  TextEditingController _noteContentController = TextEditingController();
+  final TextEditingController _noteTitleController = TextEditingController();
+  final TextEditingController _noteContentController = TextEditingController();
+  String date = DateTime.now().toString();
 
   @override
   Widget build(BuildContext context) {
@@ -24,92 +26,111 @@ class _AddNoteState extends State<AddNote> {
         elevation: 0.0,
         title: const Text('Add new note'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await FirebaseFirestore.instance.collection('notes').add({
+                'note_title': _noteTitleController.text,
+                'note_content': _noteContentController.text,
+                'creation_date': date,
+                'color_id': color_id,
+              }).then((value) => Navigator.pop(context));
+            },
+            icon: const Icon(Icons.done_all),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Title',
-              style: AppStyle.mainTitle,
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            TextField(
-              controller: _noteTitleController,
-              decoration: const InputDecoration(
-                hintText: 'Enter title',
-                border: InputBorder.none,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Title',
+                style: AppStyle.mainTitle,
               ),
-            ),
-            const SizedBox(
-              height: 18.0,
-            ),
-            Text(
-              'Content',
-              style: AppStyle.mainTitle,
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            TextField(
-              controller: _noteContentController,
-              decoration: const InputDecoration(
-                hintText: 'Enter content',
-                border: InputBorder.none,
+              const SizedBox(
+                height: 10.0,
               ),
-            ),
-            const SizedBox(
-              height: 18.0,
-            ),
-            Text(
-              'Color',
-              style: AppStyle.mainTitle,
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  for (int i = 0; i < AppStyle.cardsColor.length; i++)
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          color_id = i;
-                        });
-                      },
-                      child: Container(
-                        width: 34.0,
-                        height: 34.0,
-                        margin: const EdgeInsets.only(right: 4.0),
-                        decoration: BoxDecoration(
-                          color: AppStyle.cardsColor[i],
-                          borderRadius: BorderRadius.circular(10.0),
+              TextField(
+                controller: _noteTitleController,
+                decoration: const InputDecoration(
+                  hintText: 'Type your note title here...',
+                  border: InputBorder.none,
+                ),
+              ),
+              const SizedBox(
+                height: 18.0,
+              ),
+              Text(
+                'Content',
+                style: AppStyle.mainTitle,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              TextField(
+                controller: _noteContentController,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  hintText: 'Type your note content here...',
+                  border: InputBorder.none,
+                ),
+              ),
+              const SizedBox(
+                height: 18.0,
+              ),
+              Text(
+                'Created on: $date',
+                style: AppStyle.dateTitle,
+              ),
+              const SizedBox(
+                height: 18.0,
+              ),
+              Text(
+                'Color',
+                style: AppStyle.mainTitle,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    for (int i = 0; i < AppStyle.cardsColor.length; i++)
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            color_id = i;
+                          });
+                        },
+                        child: Container(
+                          width: 34.0,
+                          height: 34.0,
+                          margin: const EdgeInsets.only(right: 4.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 1.0,
+                            ),
+                            color: AppStyle.cardsColor[i],
+                            borderRadius: BorderRadius.circular(100),
+                          ),
                         ),
                       ),
-                    ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 18.0,
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(MediaQuery.of(context).size.width, 42.0),
+                  ],
                 ),
-                child: const Text('Add note'),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 18.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
